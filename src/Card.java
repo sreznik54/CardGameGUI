f177394db22f067ca7ae2b13563ff5241e18f7f5
Sam Reznik <sam.reznik@menloschool.org>
2023-11-28 08:21:19 -0800

    wrote the card class with a constuctor, get and setter methods, and a toString

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,38 @@
 public class Card {
     private String suit;
+    private String rank;
+    private int point;
 
-    public Card()
+    public Card(String rank, String suit, int point)
     {
-        suit = "Diamonds";
+        this.suit = suit;
+        this.rank = rank;
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    public void setRank(String rank){
+        this.rank = rank;
+    }
+    public void setSuit(String suit){
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,3 @@ public class Main {
     public static void main(String[] args) {
         }
     }
-}
