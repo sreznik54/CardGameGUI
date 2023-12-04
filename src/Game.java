6e557b749b997fbbb5decc2273011d1272f3f1dd
Sam Reznik <sam.reznik@menloschool.org>
2023-12-04 11:53:24 -0800

    finished deck

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
 public class Game {
+    private Deck deck;
+    private Player player;
+    public Game() {
+        String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+        deck = new Deck(ranks, suits, values);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter player's name: ");
+        String playerName = scanner.nextLine();
+        player = new Player(playerName);
+    }
+    public void printInstructions() {
+        System.out.println("Welcome to Blackjack!");
+        System.out.println("Try to get as close to 21 without going over!");
+        System.out.println();
+    }
+
+    public void playGame(){
+        printInstructions();
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.playGame();
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<Card> hand;
+    private int points;
+    private String name;
+
+    public Player(String name)
+    {
+        this.points = 1000;
+        this.name = name;
+        hand = new ArrayList<Card>();
+    }
+
+    public void addCard(Card newCard){
+        hand.add(newCard);
+    }
+    public void addPoints(int points) {
+        this.points += points;
+    }
+
+
 }
